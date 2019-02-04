b6118aac39ae3ebfbb57460193c01cbdf8c1251f
Jathin Nagesh <43653415+IamJathin@users.noreply.github.com>
2019-02-04 19:24:36 +0100

    Add files via upload

---
+++ twitterAnalytics.R
@@ -0,0 +1,41 @@
+#load library
+library(twitteR)
+library(tm)
+library(syuzhet)
+library(SnowballC)
+
+
+#load credentials
+consumer_key <- "oL7Rw9ldFHMfEEB73W7RUpGAz"
+consumer_secret<- "f8HzUWJqQzMuJkI2zjrSLoWBZhbewb8hMOeqeY58gJDKeItrtr"
+access_token <- "1064488435560448000-bAm2w0NqBYgSoxHgcgpjlJoWugdHAf"
+access_secret <- "XcQH7UzaIppJiafQTWQ9ljboUrDKRJ8PelWJSqraL88kk"
+#set up to authenticate
+setup_twitter_oauth(consumer_key ,consumer_secret,access_token ,access_secret)
+
+tweets <- searchTwitter("#souq", n=10000, lang = "en")
+tweets <- strip_retweets(tweets)
+#tweets <- userTimeline("narendramodi", n=1000)
+tweets.df <- twListToDF(tweets) 
+
+#tweets.df2 <- gsub("http.*","",tweets.df$text)
+#tweets.df2 <- gsub("https.*","",tweets.df2)
+#tweets.df2 <- gsub("#.*","",tweets.df2)
+#tweets.df2 <- gsub("@.*","",tweets.df2)
+
+
+word.df <- as.vector(tweets.df2)
+
+emotion.df <- get_nrc_sentiment(word.df)
+
+emotion.df2 <- cbind(tweets.df2, emotion.df) 
+
+sent.value <- get_sentiment(word.df)
+
+most.positive <- word.df[sent.value == max(sent.value)]
+
+most.negative <- word.df[sent.value <= min(sent.value)] 
+most.negative
+
+category_senti <- ifelse(sent.value < 0, "Negative", ifelse(sent.value > 0, "Positive", "Neutral"))
+table(category_senti)
